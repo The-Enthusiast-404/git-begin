e09ddf9decb6bdca8290d3992d5cbe0dddd7d22e
omjogani <om080jogani@gmail.com>
2024-08-17 15:49:37 +0530

    Added: Dark/Light Mode along with its configurations | Minor changes in root file

--- app/root.tsx
+++ app/root.tsx
@@ -1,30 +1,46 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react"
 import "./tailwind.css"
+import clsx from "clsx"
+import { PreventFlashOnWrongTheme, ThemeProvider, useTheme } from "remix-themes"
 
-export function Layout({ children }: { children: React.ReactNode }) {
+import { themeSessionResolver } from "./sessions.server"
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "@remix-run/react"
+import { LoaderFunctionArgs } from "@remix-run/node"
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { getTheme } = await themeSessionResolver(request)
+  return {
+    theme: getTheme(),
+  }
+}
+
+export default function AppWithProviders() {
+  const data = useLoaderData<typeof loader>()
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+      <App />
+    </ThemeProvider>
   )
 }
 
-export default function App() {
-  return <Outlet />
+export function App() {
+  const data = useLoaderData<typeof loader>()
+  const [theme] = useTheme()
+  return (
+    <html lang="en" className={clsx(theme)}>
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <Meta />
+      <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
+      <Links />
+      <title>Git Begin</title>
+    </head>
+    <body>
+    <Outlet />
+    <ScrollRestoration />
+    <Scripts />
+    <LiveReload />
+    </body>
+    </html>
+  )
 }
