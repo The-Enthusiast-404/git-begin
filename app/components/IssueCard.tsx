1604fb81af1fefa5c3ab1749c83512d5e2df397c
omjogani <om080jogani@gmail.com>
2024-08-20 22:36:32 +0530

    Update: Minor changes in IssueCard | Added Tech Icons for better experience

--- app/components/IssueCard.tsx
+++ app/components/IssueCard.tsx
@@ -1,9 +1,8 @@
-import { Issue } from "../types"
+import { Issue } from "~/types"
 import { Card, CardContent } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import { Separator } from "@/components/ui/separator"
 import { Star, Calendar, Code, Tag, MessageSquare } from "lucide-react"
-import { ModeToggle } from "~/components/toggle-menu"
 
 type IssueCardProps = {
   issue: Issue
@@ -25,7 +24,43 @@ const getBeginnerFriendlyLabel = (labels: string[]): string => {
 
 export function IssueCard({ issue }: IssueCardProps) {
   return (
-    <Card className="hover:shadow-lg transition-shadow duration-300">
+    // <Card className="hover:shadow-lg transition-shadow duration-300">
+    //   <CardContent className="p-6">
+    //     <div className="flex items-center justify-between">
+    //       <a
+    //         href={issue.html_url}
+    //         className="text-lg font-semibold text-blue-600 hover:underline truncate max-w-[80%]"
+    //       >
+    //         {issue.title}
+    //       </a>
+    //       <Badge variant="secondary">
+    //         {getBeginnerFriendlyLabel(issue.labels)}
+    //       </Badge>
+    //     </div>
+    //     <Separator className="my-4" />
+    //     <div className="flex flex-wrap items-center gap-4 text-sm text-gray-500">
+    //       <span className="flex items-center">
+    //         <Star className="w-4 h-4 mr-1" /> {issue.stars_count}
+    //       </span>
+    //       <span className="flex items-center">
+    //         <Code className="w-4 h-4 mr-1" /> {issue.language || "N/A"}
+    //       </span>
+    //       <span className="flex items-center">
+    //         <Calendar className="w-4 h-4 mr-1" />{" "}
+    //         {new Date(issue.created_at).toLocaleString()}
+    //       </span>
+    //       <span className="flex items-center">
+    //         <Tag className="w-4 h-4 mr-1" /> {issue.labels.join(", ")}
+    //       </span>
+    //       <span className="flex items-center">
+    //         <MessageSquare className="w-4 h-4 mr-1" /> {issue.comments_count}
+    //       </span>
+    //       {issue.is_assigned && <Badge variant="outline">Assigned</Badge>}
+    //     </div>
+    //     <p className="mt-2 text-sm text-gray-600">{issue.repository_name}</p>
+    //   </CardContent>
+    // </Card>
+    <Card className="border rounded-md hover:shadow-xl transition-shadow duration-300">
       <CardContent className="p-6">
         <div className="flex items-center justify-between">
           <a
@@ -37,30 +72,30 @@ export function IssueCard({ issue }: IssueCardProps) {
           <Badge variant="secondary">
             {getBeginnerFriendlyLabel(issue.labels)}
           </Badge>
-          <ModeToggle />
         </div>
         <Separator className="my-4" />
         <div className="flex flex-wrap items-center gap-4 text-sm text-gray-500">
+      <span className="flex items-center">
+        <Star className="w-4 h-4 mr-1" /> {issue.stars_count}
+      </span>
           <span className="flex items-center">
-            <Star className="w-4 h-4 mr-1" /> {issue.stars_count}
-          </span>
+        <Code className="w-4 h-4 mr-1" /> {issue.language || "N/A"}
+      </span>
           <span className="flex items-center">
-            <Code className="w-4 h-4 mr-1" /> {issue.language || "N/A"}
-          </span>
-          <span className="flex items-center">
-            <Calendar className="w-4 h-4 mr-1" />{" "}
+        <Calendar className="w-4 h-4 mr-1" />
             {new Date(issue.created_at).toLocaleString()}
-          </span>
+      </span>
           <span className="flex items-center">
-            <Tag className="w-4 h-4 mr-1" /> {issue.labels.join(", ")}
-          </span>
+        <Tag className="w-4 h-4 mr-1" /> {issue.labels.join(", ")}
+      </span>
           <span className="flex items-center">
-            <MessageSquare className="w-4 h-4 mr-1" /> {issue.comments_count}
-          </span>
+        <MessageSquare className="w-4 h-4 mr-1" /> {issue.comments_count}
+      </span>
           {issue.is_assigned && <Badge variant="outline">Assigned</Badge>}
         </div>
         <p className="mt-2 text-sm text-gray-600">{issue.repository_name}</p>
       </CardContent>
     </Card>
+
   )
 }
---
+++ app/icons/index.tsx
@@ -0,0 +1,25 @@
+import { FaReact } from "react-icons/fa6"
+import { RiFlutterFill } from "react-icons/ri";
+import { SiMdx } from "react-icons/si";
+import { RiAngularjsLine } from "react-icons/ri";
+import { FaNodeJs } from "react-icons/fa";
+import { FaVuejs } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { FaDocker } from "react-icons/fa";
+import { FaPython } from "react-icons/fa";
+import { FaJava } from "react-icons/fa";
+import { FaPhp } from "react-icons/fa";
+import { FaAws } from "react-icons/fa";
+
+export const NodeJsIcon = () => <FaNodeJs />;
+export const VueJsIcon = () => <FaVuejs />;
+export const TypeScriptIcon = () => <SiTypescript />;
+export const DockerIcon = () => <FaDocker />;
+export const PythonIcon = () => <FaPython />;
+export const JavaIcon = () => <FaJava />;
+export const PhpIcon = () => <FaPhp />;
+export const AwsIcon = () => <FaAws />;
+export const ReactIcon = () => <FaReact />
+export const FlutterIcon = () => <RiFlutterFill />
+export const MdxIcon = () => <SiMdx />
+export const AngularIcon = () => <RiAngularjsLine />
