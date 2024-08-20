cbc6d67c71a25a9d8867621acf1ddb70a1f70c18
omjogani <om080jogani@gmail.com>
2024-08-20 22:35:30 +0530

    Added: Dark/Light mode toggle button

--- app/root.tsx
+++ app/root.tsx
@@ -30,6 +30,10 @@ export function App() {
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Edu+VIC+WA+NT+Beginner:wght@400..700&display=swap"
+            rel="stylesheet" />
       <Meta />
       <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
       <Links />
