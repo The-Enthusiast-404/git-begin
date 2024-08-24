6e1d7fb5bea2469fa31eb4b2c39239b149c2783e
sahitya-chandra <sahityajb@gmail.com>
2024-08-25 00:01:19 +0530

    categories.ts updated

--- app/data/categories.ts
+++ app/data/categories.ts
@@ -1,11 +1,16 @@
 const categories = [
   { value: "all", label: "All Categories" },
+  { value: "education", label: "Education" },
   { value: "web-dev", label: "Web Development" },
   { value: "mobile-dev", label: "Mobile Development" },
+  { value: "ios-dev", label: "Ios Develpment" },
+  { value: "blockchain-dev", label: "Blockchain Development" },
   { value: "data-science", label: "Data Science" },
+  { value: "artificial-intelligence", label: "Artificial Intelligence" },
   { value: "machine-learning", label: "Machine Learning" },
   { value: "devops", label: "DevOps" },
   { value: "cybersecurity", label: "Cybersecurity" },
+  { value: "operating-system", label: "Operating System" },
   { value: "documentation", label: "Documentation" },
 ]
 
