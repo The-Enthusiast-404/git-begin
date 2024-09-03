2df0f7c993fa38e9fcd344c6bb24806f422f83ce
rodriveiga01 <rodriveiga01@gmail.com>
2024-09-03 16:58:32 +0100

    changed for loop to map

--- app/hooks/useCategories.ts
+++ app/hooks/useCategories.ts
@@ -1,17 +1,14 @@
 import { categories } from "~/data/categories";
 import { useTranslatedText } from "~/locale/languageUtility";
 
-export function useCategories(){
-    const rawCategories = categories
-    const t = useTranslatedText()
+export function useCategories() {
+    const rawCategories = categories;
+    const t = useTranslatedText();
 
-    let translateCategories: typeof categories = []
+    const translatedCategories = rawCategories.map(category => ({
+        ...category,
+        label: t(category.label)
+    }));
 
-    for (let category of rawCategories) {
-        let realLabel = t(category.label)
-        category.label = realLabel
-        translateCategories.push(category)
-    }
-
-    return translateCategories
+    return translatedCategories;
 }
