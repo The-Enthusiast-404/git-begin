153548c5c28be57ab773731de745a7db9e8c61f5
omjogani <om080jogani@gmail.com>
2024-09-03 13:04:10 +0530

    Added: Jest Config for testing

---
+++ __tests__/services/github.test.ts
@@ -0,0 +1,12 @@
+import { fetchGitHubIssues, fetchGitHubIssuesByCategory, fetchGitHubIssuesByFramework } from "~/services/github"
+import { afterEach, beforeEach, describe, expect, it, jest } from "@jest/globals"
+
+import { mock } from "jest-mock-extended"
+
+jest.mock("@octokit/graphql")
+
+describe("", () => {
+  it("should calculate correct sum", () => {
+    expect(1 + 2).toBe(3)
+  })
+})
